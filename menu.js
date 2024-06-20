4d63e216b43339b6e14842af67e084cea98f3125
Arthur <153244771+ArthurAGomes@users.noreply.github.com>
2024-06-20 18:44:17 +0000

    removendo

--- menu.js
+++ menu.js
@@ -5,33 +5,3 @@ $(document).ready(function(){
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav ul li');
-
-    const makeActive = (id) => {
-        navLinks.forEach(link => {
-            if (link.querySelector('a').getAttribute('href') === `#${id}`) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    };
-
-    const options = {
-        threshold: 0.6
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                makeActive(entry.target.id);
-            }
-        });
-    }, options);
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-});
